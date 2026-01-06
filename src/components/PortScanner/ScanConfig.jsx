f9587e24240fed44e3bb7264d5d7358c91ae5f22
dalbit <dalbitfox@gmail.net>
2026-01-07 03:40:26 +0900

    chore: update scan config panel title

--- src/components/PortScanner/ScanConfig.jsx
+++ src/components/PortScanner/ScanConfig.jsx
@@ -20,7 +20,7 @@ const ScanConfig = ({
                     <circle cx="12" cy="12" r="10" />
                     <path d="M2 12h20M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
                 </svg>
-                <span className="panel-title">스캔 설정</span>
+                <span className="panel-title">스캔 정보 입력</span>
             </div>
 
             <div className="form-grid">
